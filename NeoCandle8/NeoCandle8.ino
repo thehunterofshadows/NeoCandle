0f26f8f3510cfc5702da1a3be2ad92f79292f9c2
thehunterofshadows <47578899+thehunterofshadows@users.noreply.github.com>
2020-10-28 08:52:57 -0500

    remove intrupt by watchdog

--- NeoCandle8/NeoCandle8.ino
+++ NeoCandle8/NeoCandle8.ino
@@ -191,70 +191,8 @@ void flutter(int f) {
 }
 
 
-//NOTE YOU COMMENTED OUT SOME OF THIS STUFF IN THIS FUNCTION, UNCOMMENT AS NEEDED
-//also this is setup for timed sleep.  You are using pin intrupt sleep so changing 
-//function for now.
-void system_sleep_old() {
-
-  //cbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter OFF
-
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
-
-  sleep_enable();
-
-  sleep_mode();                        // System sleeps here
-
-  sleep_disable();                     // System continues execution here when watchdog timed out 
-
-  //sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
-
-}
-
-// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
-
-// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
-void setup_watchdog(int ii) {
-
-  byte bb;
-
-  int ww;
-
-  if (ii > 9 ) ii=9;
-
-  bb=ii & 7;
-
-  if (ii > 7) bb|= (1<<5);
-
-  bb|= (1<<WDCE);
-
-  ww=bb;
-
-  MCUSR &= ~(1<<WDRF);
-
-  // start timed sequence
-
-  WDTCR |= (1<<WDCE) | (1<<WDE);
-
-  // set new watchdog timeout value
-
-  WDTCR = bb;
-
-  WDTCR |= _BV(WDIE);
-
-}
-
-// Watchdog Interrupt Service / is executed when watchdog timed out
-
-ISR(WDT_vect) {
-
-  counter++;
-
-  f_wdt=1;  // set global flag
-
-}
 
 
-// From http://interface.khm.de/index.php/lab/experiments/sleep_watchdog_battery/
 void system_sleep() {
   cbi(ADCSRA,ADEN); // Switch Analog to Digital converter OFF
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Set sleep mode
