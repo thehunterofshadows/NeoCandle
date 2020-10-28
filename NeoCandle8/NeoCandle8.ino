b204d1f86f885e42c52c472770dafbd40bb51606
thehunterofshadows <47578899+thehunterofshadows@users.noreply.github.com>
2020-10-28 07:24:23 -0500

    updates and working on intrupt by pin

--- NeoCandle8/NeoCandle8.ino
+++ NeoCandle8/NeoCandle8.ino
@@ -3,16 +3,29 @@
 // by Tim Bartlett, December 2013
 
 #include <Adafruit_NeoPixel.h>
+#include <avr/sleep.h>
 #define PIN 4
-#define pixels 9
-int bluePx = 10;
+#define PIXELS 9
 
-// animation time variables, with recommendations
-int burnDepth = 6; //how much green dips below grnHigh for normal burn - 
+//setup using button to wake
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+
+int counter = 0;
+volatile boolean f_wdt = 1;
 
 // color variables: mix RGB (0-255) for desired yellow
 int redPx = 255;
 int grnHigh = 100;
+int bluePx = 10;
+
+// animation time variables, with recommendations
+int burnDepth = 6; //how much green dips below grnHigh for normal burn - 
 int flutterDepth = 20; //maximum dip for flutter
 int cycleTime = 120; //duration of one dip in milliseconds
 
@@ -27,7 +40,7 @@ int flickLow;
 int flutDelay;
 int flutLow;
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixels, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   flickerDepth = (burnDepth + flutterDepth) / 2.4;
@@ -37,9 +50,15 @@ void setup() {
   flickDelay = (cycleTime / 2) / flickerDepth;
   flutLow = grnHigh - flutterDepth;
   flutDelay = ((cycleTime / 2) / flutterDepth);
-  
+  setup_watchdog(8); // approximately 4 seconds sleep  
   strip.begin();
   strip.show();
+
+  //setup pin1 to be the intrupt
+  pinMode(1,INPUT);
+  
+  sbi(GIMSK,PCIE); // Turn on Pin Change interrupt
+  sbi(PCMSK,PCINT1); // Which pins are affected by the interrupt
 }
 
 // In loop, call CANDLE STATES, with duration in seconds
@@ -50,6 +69,21 @@ void setup() {
 
 void loop() {
   burn(10);
+  
+  //after running for a bit goto sleep until button pushed
+  system_sleep();
+
+  //strip.fill(strip.Color(250,0,0));
+  //strip.show();
+  //delay(2000);
+  //delay(6000);
+  //strip.fill(strip.Color(0,0,0));
+  //strip.show();
+  //set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
+  //sleep_enable();                          // enables the sleep bit in the mcucr register so sleep is possible
+  //sleep_mode();                          // here the device is actually put to sleep!!
+  //delay(6000);
+  //system_sleep();
   flicker(5);
   burn(8);
   flutter(6);
@@ -57,6 +91,7 @@ void loop() {
   on(10);
   burn(10);
   flicker(10);
+
 }
 
 
@@ -70,11 +105,11 @@ void fire(int grnLow) {
     strip.setPixelColor(1, redPx-180, darkGrn, 0);
     strip.setPixelColor(2, redPx-120, grnPx-50, bluePx-5);
     strip.setPixelColor(3, redPx-60, grnPx-35, bluePx-2);
-    strip.setPixelColor(4, redPx-180, darkGrn, 0);
-    strip.setPixelColor(5, redPx-180, darkGrn, 0);
-    strip.setPixelColor(6, redPx-120, grnPx-50, bluePx-5);
-    strip.setPixelColor(7, redPx-60, grnPx-35, bluePx-2);
-    strip.setPixelColor(8, redPx-180, darkGrn, 0);
+    strip.setPixelColor(4, redPx, grnPx, bluePx);
+    strip.setPixelColor(5, redPx, grnPx, bluePx);
+    strip.setPixelColor(6, redPx, halfGrn, bluePx);
+    strip.setPixelColor(7, redPx, grnHigh, bluePx);
+    strip.setPixelColor(8, redPx, grnHigh, bluePx);
     strip.show();
     delay(fDelay);
   }  
@@ -86,11 +121,11 @@ void fire(int grnLow) {
     strip.setPixelColor(1, redPx-180, darkGrn, 0);
     strip.setPixelColor(2, redPx-120, grnPx-50, bluePx-5);
     strip.setPixelColor(3, redPx-60, grnPx-35, bluePx-2);
-    strip.setPixelColor(4, redPx-180, darkGrn, 0);
-    strip.setPixelColor(5, redPx-180, darkGrn, 0);
-    strip.setPixelColor(6, redPx-120, grnPx-50, bluePx-5);
-    strip.setPixelColor(7, redPx-60, grnPx-35, bluePx-2);
-    strip.setPixelColor(8, redPx-180, darkGrn, 0);
+    strip.setPixelColor(4, redPx, grnPx, bluePx);
+    strip.setPixelColor(5, redPx, grnPx, bluePx);
+    strip.setPixelColor(6, redPx, halfGrn, bluePx);
+    strip.setPixelColor(7, redPx, grnHigh, bluePx);
+    strip.setPixelColor(8, redPx, grnHigh, bluePx);
     strip.show();
     delay(fDelay);
   }
@@ -104,11 +139,11 @@ void on(int f) {
     strip.setPixelColor(1, redPx-180, grnPx-70, 0);
     strip.setPixelColor(2, redPx-120, grnPx-50, bluePx-5);
     strip.setPixelColor(3, redPx-60, grnPx-35, bluePx-2);
-    strip.setPixelColor(4, redPx-180, grnPx-70, 0);
-    strip.setPixelColor(5, redPx-180, grnPx-70, 0);
-    strip.setPixelColor(6, redPx-120, grnPx-50, bluePx-5);
-    strip.setPixelColor(7, redPx-60, grnPx-35, bluePx-2);
-    strip.setPixelColor(8, redPx-180, grnPx-70, 0);
+    strip.setPixelColor(4, redPx, grnPx, bluePx);
+    strip.setPixelColor(5, redPx, grnPx, bluePx);
+    strip.setPixelColor(6, redPx, grnPx, bluePx);
+    strip.setPixelColor(7, redPx, grnHigh, bluePx);
+    strip.setPixelColor(8, redPx, grnHigh, bluePx);
   strip.show();
   delay(fRep);
 }
@@ -151,4 +186,79 @@ void flutter(int f) {
   fDelay = burnDelay;
   fire(burnLow);
   fire(burnLow);
+}
+
+
+//NOTE YOU COMMENTED OUT SOME OF THIS STUFF IN THIS FUNCTION, UNCOMMENT AS NEEDED
+//also this is setup for timed sleep.  You are using pin intrupt sleep so changing 
+//function for now.
+void system_sleep_old() {
+
+  //cbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter OFF
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+
+  sleep_enable();
+
+  sleep_mode();                        // System sleeps here
+
+  sleep_disable();                     // System continues execution here when watchdog timed out 
+
+  //sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
+
+}
+
+// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+void setup_watchdog(int ii) {
+
+  byte bb;
+
+  int ww;
+
+  if (ii > 9 ) ii=9;
+
+  bb=ii & 7;
+
+  if (ii > 7) bb|= (1<<5);
+
+  bb|= (1<<WDCE);
+
+  ww=bb;
+
+  MCUSR &= ~(1<<WDRF);
+
+  // start timed sequence
+
+  WDTCR |= (1<<WDCE) | (1<<WDE);
+
+  // set new watchdog timeout value
+
+  WDTCR = bb;
+
+  WDTCR |= _BV(WDIE);
+
+}
+
+// Watchdog Interrupt Service / is executed when watchdog timed out
+
+ISR(WDT_vect) {
+
+  counter++;
+
+  f_wdt=1;  // set global flag
+
+}
+
+
+// From http://interface.khm.de/index.php/lab/experiments/sleep_watchdog_battery/
+void system_sleep() {
+  cbi(ADCSRA,ADEN); // Switch Analog to Digital converter OFF
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Set sleep mode
+  sleep_mode(); // System sleeps here
+  sbi(ADCSRA,ADEN);  // Switch Analog to Digital converter ON
+}
+
+ISR(PCINT0_vect) {
 }
